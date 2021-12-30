93a84a0a8712b155be24ac2f306fd2ca85e1a902
samgliu <icefire587@gmail.com>
2021-12-30 14:32:20 -0500

    save

--- index.js
+++ index.js
@@ -20,9 +20,18 @@ function normalizePort(val) {
 const io = require('socket.io')(socketPort, {
     cors: {
         // white lists
-        origin: '*',
-        secure: true,
-        credentials: true,
+        origin: [
+            'http://localhost:3000',
+            'http://localhost:3001',
+            'http://127.0.0.1:3000',
+            'http://172.19.133.104:3001',
+            'http://172.19.133.104:3000',
+            'https://samgliu.github.io',
+            'http://samgliu.github.io',
+        ],
+        methods: 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+        allowedHeaders: 'Content-Type, Accept, X-Access-Token, X-Refresh-Token',
+        //credentials: true,
     },
 });
 
